554a75e7b053560eac481f0711d1d8b3b9cf9364
bdandurand <bdandurand@anl.gov>
2019-03-21 15:55:01 -0500

    Initial editing of algorithmic description in comments.

--- src/ADMM.jl
+++ src/ADMM.jl
@@ -14,21 +14,22 @@ The augmented Lagrange dual (ALD) of SMIP is given by
   subject to (x_j,y_j) \in G_j for all j.
 
 ADMM to solve the ALD is as follows:
+0. Obtain an initial cut for the MP
 REPEAT the following steps.
 1. For given \lambda_j^k and z^k, solve the ALD to find (x_j^{k+1},y_j^{k+1}).
 2. For given \lambda_j^k and (x_j^{k+1},y_j^{k+1}), solve the ALD w.r.t. z, which has
 the closed-form solution:
   \sum_{j=1}^N [\rho (z^{k+1} - x_j^k) - \lambda_j^k] = 0;
   that is, z^{k+1} = \sum_{j=1}^N \lambda_j^k / (N \rho) + \sum_{j=1}^N x_j^k / N
-3. \lambda_j^{k+1} = \lambda_j^k - \rho (z^{k+1} - x_j^k)
-
-Frank-Wolfe algorithm for step 1:
-REPEAT the following steps.
-1. For given (x_j^k,z^k,\lambda_j^k), solve
-  min p_j (c^T u_j + d_j^T v_j) + (\lambda_j^k)^T u_j - \rho (z^k - x_j^k)^T u_j
-  subject to
-  (u_j,v_j) \in G_j
-2. Find a step size \gamma (should be in a closed form).
+3. Conditionally update lambda and update model
+  a. Compute provisional \widetilde{\lambda}_j^{k+1} = \lambda_j^k - \rho (z^{k+1} - x_j^k)
+  b. Solve Lagrange (column generation) subproblem with \lambda=\widetilde{\lambda}^{k+1}
+     min p_j (c^T u_j + d_j^T v_j) + (\lambda_j^k + \rho( x_j^k - z^k ))^T u_j 
+     subject to
+     (u_j,v_j) \in G_j
+     use the solution (u^*,v^*) to update the MP model, adding cuts and possibly aggregating previous cuts, 
+     use the optimal value \widetilde{\phi} to test the serious step condition (SSC)
+  c. If SSC passes, set \lambda^{k+1} = \widetilde{\lambda}^{k+1}, otherwise \lambda^{k+1} = \lambda^k
 =#
 
 abstract type AdmmMethod <: AbstractMethod end

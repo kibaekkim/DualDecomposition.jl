2ca441675370801d48ee59fa023e93966a59a2d0
Kibaek Kim <kimk@anl.gov>
2019-03-22 11:46:15 -0500

    modified the master solver

--- examples/farmer.jl
+++ examples/farmer.jl
@@ -1,5 +1,5 @@
 using JuDD
-using JuMP, CPLEX
+using JuMP, CPLEX, Ipopt
 
 const NS = 3  # number of scenarios
 const probability = ones(3) / 3
@@ -29,7 +29,7 @@ function main_farmer()
     JuDD.set_nonanticipativity_vars(nonanticipativity_vars())
 
     # Solve the problem with the solver; this solver is for the underlying bundle method.
-    JuDD.solve(CplexSolver(CPX_PARAM_SCRIND=0,CPX_PARAM_QPMETHOD=2), master_alrogithm = :ProximalDualBundle)
+    JuDD.solve(IpoptSolver(print_level=0), master_alrogithm = :ProximalDualBundle)
 end
 
 # This creates a Lagrange dual problem for each scenario s.

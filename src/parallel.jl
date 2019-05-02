18d716353043ea7df87babb2cb04f6ff20abb7b2
Kibaek Kim <kimk@anl.gov>
2019-05-01 21:27:11 -0500

    wrappers for some MPI functions

--- src/parallel.jl
+++ src/parallel.jl
@@ -2,17 +2,28 @@ module parallel
 using Compat
 using MPI
 
-myid = 0
-nprocs = 1
-
 function init()
     MPI.Init()
-    global myid = MPI.Comm_rank(MPI.COMM_WORLD)
-    global nprocs = MPI.Comm_size(MPI.COMM_WORLD)
 end
 
 function finalize()
     MPI.Finalize()
+end
+
+function myid()
+    if MPI.Initialized()
+        return MPI.Comm_rank(MPI.COMM_WORLD)
+    else
+        return 0
+    end
+end
+
+function nprocs()
+    if MPI.Initialized()
+        return MPI.Comm_size(MPI.COMM_WORLD)
+    else
+        return 1
+    end
 end
 
 function getpartition()
@@ -20,14 +31,14 @@ function getpartition()
 end
 
 function partition(part)
-    global mylist = Array{Array{Int64,1},1}(undef, nprocs)
-    for i in 1:nprocs
+    global mylist = Array{Array{Int64,1},1}(undef, nprocs())
+    for i in 1:nprocs()
         mylist[i] = Array{Int64,1}(undef, 0)
     end
     for i in 1:part
-        push!(mylist[(i-1)÷(part÷nprocs)+1], i)
+        push!(mylist[(i-1)÷(part÷nprocs())+1], i)
     end
-    global partitionlist = mylist[myid+1]
+    global partitionlist = mylist[myid()+1]
 end
 
 function reduce(x::Array{T,1}) where {T<:Number}
@@ -48,7 +59,7 @@ function reduce(x::Array{Float64,2})
     ret=Array{Float64,2}(undef, countssum, size(x,2))
     for i in 1:countssum
         for j in 1:size(x,2)
-            ret[i,j]=res[(i-1)*size(x,2)+j] 
+            ret[i,j]=res[(i-1)*size(x,2)+j]
         end
     end
     ret

4a7a40ce222f293d573db8d8e45f6d5449f05a73
Kibaek Kim <kimk@anl.gov>
2020-07-16 09:38:58 -0500

    test mpi code with single process

--- test/runtests.jl
+++ test/runtests.jl
@@ -190,10 +190,10 @@ end
 
 @testset "MPI tests" begin
     testdir = @__DIR__
-    # mpiexec(cmd ->run(`$cmd -np 1 $(Base.julia_cmd()) $(joinpath(testdir, "parallel.jl"))`))
+    run(`$(Base.julia_cmd()) $(joinpath(testdir, "parallel.jl"))`)
     mpiexec(cmd ->run(`$cmd -np 2 $(Base.julia_cmd()) $(joinpath(testdir, "parallel.jl"))`))
     mpiexec(cmd ->run(`$cmd -np 3 $(Base.julia_cmd()) $(joinpath(testdir, "parallel.jl"))`))
-    # mpiexec(cmd ->run(`$cmd -np 1 $(Base.julia_cmd()) $(joinpath(testdir, "../examples/farmer_mpi.jl"))`))
+    run(`$(Base.julia_cmd()) $(joinpath(testdir, "../examples/farmer_mpi.jl"))`)
     mpiexec(cmd ->run(`$cmd -np 2 $(Base.julia_cmd()) $(joinpath(testdir, "../examples/farmer_mpi.jl"))`))
     mpiexec(cmd ->run(`$cmd -np 3 $(Base.julia_cmd()) $(joinpath(testdir, "../examples/farmer_mpi.jl"))`))
 end

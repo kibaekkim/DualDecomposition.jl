50dc77be48873a023d0bdf45484be1900523afb8
Kibaek Kim <kimk@anl.gov>
2020-07-16 22:04:47 -0500

    added timing info in test

--- examples/farmer_mpi.jl
+++ examples/farmer_mpi.jl
@@ -41,11 +41,11 @@ end
 algo = DD.LagrangeDual()
 
 # partition scenarios into processes
-parallel.partition(NS)
+@time parallel.partition(NS)
 
 # Add Lagrange dual problem for each scenario s.
 models = Dict{Int,JuMP.Model}(s => create_scenario_model(s) for s in parallel.getpartition())
-for s in parallel.getpartition()
+@time for s in parallel.getpartition()
     DD.add_block_model!(algo, s, models[s])
 end
 
@@ -59,7 +59,7 @@ for s in parallel.getpartition()
 end
 
 # Set nonanticipativity variables as an array of symbols.
-DD.set_coupling_variables!(algo, coupling_variables)
+@time DD.set_coupling_variables!(algo, coupling_variables)
 
 # Solve the problem with the solver; this solver is for the underlying bundle method.
 DD.run!(algo, optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0))

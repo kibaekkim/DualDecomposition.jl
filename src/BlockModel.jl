da0d6f47dfa5e1e0edd071d29fc8614f863bdf4a
Hideaki <h.nakao1992blanca@gmail.com>
2020-07-24 02:39:24 -0400

    created AbstractBlockModel for variable access

--- src/BlockModel.jl
+++ src/BlockModel.jl
@@ -31,7 +31,9 @@ Block model struture contrains a set of `JuMP.Model` objects, each of which
 represents a sub-block model with the information of how to couple these block
 models. 
 """
-mutable struct BlockModel
+abstract type AbstractBlockModel end
+
+mutable struct BlockModel <: AbstractBlockModel
     model::Dict{Int,JuMP.Model} # Dictionary of block models
     coupling_variables::Vector{CouplingVariableRef} # array of variables that couple block models
     variables_by_couple::Dict{Any,Vector{CouplingVariableKey}} # maps `couple_id` to `CouplingVariableKey`
@@ -56,60 +58,60 @@ end
 """
     add_block_model!
 
-Add block model `model` to `block_model::BlockModel` with `block_id`.
+Add block model `model` to `block_model::AbstractBlockModel` with `block_id`.
 """
-function add_block_model!(block_model::BlockModel, block_id::Integer, model::JuMP.Model)
+function add_block_model!(block_model::AbstractBlockModel, block_id::Integer, model::JuMP.Model)
     block_model.model[block_id] = model
 end
 
 """
     num_blocks
 
-Number of blocks in `block_model::BlockModel`
+Number of blocks in `block_model::AbstractBlockModel`
 """
-num_blocks(block_model::BlockModel) = length(block_model.model)
+num_blocks(block_model::AbstractBlockModel) = length(block_model.model)
 
 """
     block_model
 
 This returns a dictionary of `JuMP.Model` objects.
 """
-block_model(block_model::BlockModel) = block_model.model
+block_model(block_model::AbstractBlockModel) = block_model.model
 
 """
     block_model
 
 This returns a `JuMP.Model` object for a given `block_id`.
 """
-block_model(block_model::BlockModel, block_id::Integer) = block_model.model[block_id]
+block_model(block_model::AbstractBlockModel, block_id::Integer) = block_model.model[block_id]
 
 """
     has_block_model
 
-This returns true if `block_model::BlockModel` has key `block_id::Integer`; false otherwise.
+This returns true if `block_model::AbstractBlockModel` has key `block_id::Integer`; false otherwise.
 """
-has_block_model(block_model::BlockModel, block_id::Integer) = haskey(block_model.model, block_id)
+has_block_model(block_model::AbstractBlockModel, block_id::Integer) = haskey(block_model.model, block_id)
 
 """
     num_coupling_variables
 
-This returns the number of coupling variables in `block_model::BlockModel`.
+This returns the number of coupling variables in `block_model::AbstractBlockModel`.
 """
-num_coupling_variables(block_model::BlockModel) = length(block_model.coupling_variables)
+num_coupling_variables(block_model::AbstractBlockModel) = length(block_model.coupling_variables)
 
 """
     coupling_variables
 
-This returns the array of coupling variables in `block_model::BlockModel`.
+This returns the array of coupling variables in `block_model::AbstractBlockModel`.
 """
-coupling_variables(block_model::BlockModel) = block_model.coupling_variables
+coupling_variables(block_model::AbstractBlockModel) = block_model.coupling_variables
 
 """
     set_coupling_variables!
 
-This sets coupling variables `variables` to `block_model::BlockModel`.
+This sets coupling variables `variables` to `block_model::AbstractBlockModel`.
 """
-function set_coupling_variables!(block_model::BlockModel, variables::Vector{CouplingVariableRef})
+function set_coupling_variables!(block_model::AbstractBlockModel, variables::Vector{CouplingVariableRef})
     block_model.coupling_variables = variables
 end
 
@@ -118,7 +120,7 @@ end
 
 This sets `BlockModel.variables_by_couple`.
 """
-function set_variables_by_couple!(block_model::BlockModel, variables::Vector{CouplingVariableKey})
+function set_variables_by_couple!(block_model::AbstractBlockModel, variables::Vector{CouplingVariableKey})
     block_model.variables_by_couple = Dict(v.coupling_id => [] for v in variables)
     for v in variables
         push!(block_model.variables_by_couple[v.coupling_id], v)
@@ -130,11 +132,11 @@ end
 
 This returns the dual objective value obtained from a method.
 """
-dual_objective_value(block_model::BlockModel) = block_model.dual_bound
+dual_objective_value(block_model::AbstractBlockModel) = block_model.dual_bound
 
 """
     dual_solution
 
 This returns a vector of dual solution obtained from a method.
 """
-dual_solution(block_model::BlockModel) = block_model.dual_solution
+dual_solution(block_model::AbstractBlockModel) = block_model.dual_solution

151a8a6e3942e85c62bac74083960ff151795677
Hideaki <h.nakao1992blanca@gmail.com>
2020-08-04 16:02:14 -0400

    created ScenarioTree

--- examples/investment.jl
+++ examples/investment.jl
@@ -4,86 +4,6 @@ using Random
 
 const DD = DualDecomposition
 
-"""
-    Scenario Tree
-"""
-
-struct TreeNode
-    parent::Int                                 # index of parent node
-    children::Vector{Int}                       # indices of child nodes
-    k::Int                                      # current stage
-    ξ::Vector{Float64}                          # current scenario
-end
-mutable struct Tree
-    nodes::Vector{TreeNode}     # list of nodes
-    K::Int                      # length of tree
-end
-
-Tree(ξ::Vector{Float64}) = 
-    Tree([TreeNode(0, Vector{Int}(), 1, ξ )], 1)
-
-function addchild!(tree::Tree, id::Int, ξ::Vector{Float64})
-    #   adds child node to tree.nodes[id]
-    1 <= id <= length(tree.nodes) || throw(BoundsError(tree, id))   # check if id is valid
-    k = get_stage(tree, id) + 1                                     # get new stage value
-    push!(tree.nodes, TreeNode(id, Vector{}(), k, ξ ))   # push to node list
-    child_id = length(tree.nodes)                                   # get current node ID
-    push!(tree.nodes[id].children, child_id)                        # push child_id to parent node children
-    if k > tree.K
-        tree.K = k  # update length of tree to the maximum value
-    end
-end
-
-get_children(tree, id) = tree.nodes[id].children
-get_parent(tree,id) = tree.nodes[id].parent
-get_stage(tree, id) = tree.nodes[id].k
-get_scenario(tree, id) = tree.nodes[id].ξ
-
-function get_history(tree::Tree, id::Int)::Array{Int}
-    # gets a vector of tree node IDs up until current
-    stage = get_stage(tree, id)
-    hist = Array{Int}(undef, stage)
-
-    current_id = id
-    for k = stage:-1:1
-        hist[k] = current_id
-        current_id = get_parent(tree, current_id)
-    end
-    return hist
-end
-
-function get_future(tree::Tree, root_id::Int)::Array{Int}
-    #   output list of all leaf node IDs branching from root_id
-    arr_leaves = Int[]
-
-    function iterate_children(tree::Tree, id::Int)
-        children = get_children(tree, id)
-        if length(children) == 0
-            #buffer output
-            push!(arr_leaves, id)
-        else
-            for child in children
-                iterate_children(tree, child)
-            end
-        end
-    end
-
-    iterate_children(tree, root_id)
-    return arr_leaves
-end
-
-function get_stage_id(tree::Tree)::Array{Array{Int}}
-    # gets a list of tree node IDs separated by stages
-    K = tree.K
-    nodelist = [ Int[] for _ in 1:K]
-
-    for id in 1:length(tree.nodes)
-        k = get_stage(tree, id)
-        push!(nodelist[k], id)
-    end
-    return nodelist
-end
-
 
 """
 a: interest rate
@@ -102,25 +22,25 @@ asset2: 1.06 or 0.94
 
 """
 
-function create_tree(K::Int, L::Int)::Tree
+function create_tree(K::Int, L::Int)::DD.Tree
     π = ones(L)                              
-    tree = Tree(π)
+    tree = DD.Tree(π)
     add_nodes!(K, L, tree, 1, 1)
     return tree
 end
 
-function add_nodes!(K::Int, L::Int, tree::Tree, id::Int, k::Int)
+function add_nodes!(K::Int, L::Int, tree::DD.Tree, id::Int, k::Int)
     if k < K-1
         ls = iterlist(L,tree.nodes[id].ξ)
         for π in ls
-            addchild!(tree, id, π)
+            DD.addchild!(tree, id, π)
             childid = length(tree.nodes)
             add_nodes!(K, L, tree, childid, k+1)
         end
     elseif k == K-1
         ls = iterlist(L,tree.nodes[id].ξ)
         for π in ls
-            addchild!(tree, id, π)
+            DD.addchild!(tree, id, π)
         end
     end
 end
@@ -182,8 +102,8 @@ const a = 0.01
 const b1 = 100  # initial capital
 const b2 = 30   # income
 
-function create_scenario_model(K::Int, L::Int, tree::Tree, id::Int)
-    hist = get_history(tree, id)
+function create_scenario_model(K::Int, L::Int, tree::DD.Tree, id::Int)
+    hist = DD.get_history(tree, id)
     m = Model(GLPK.Optimizer) 
     #@variable(m, x[1:K,1:L], integer=true)
     @variable(m, x[1:K,1:L])
@@ -216,15 +136,6 @@ function create_scenario_model(K::Int, L::Int, tree::Tree, id::Int)
     return m
 end
 
-function leaf2block(nodes::Array{Int})::Dict{Int,Int}
-    leafdict = Dict{Int,Int}()
-    for i in 1:length(nodes)
-        id = nodes[i]
-        leafdict[id] = i
-    end
-    return leafdict
-end
-
 """
 
 The main computation section
@@ -239,14 +150,13 @@ function main_comp()
 end
 
 
-function dual_decomp(L::Int, tree::Tree)
+function dual_decomp(L::Int, tree::DD.Tree)
     # Create DualDecomposition instance.
     algo = DD.LagrangeDual(BM.TrustRegionMethod)
-    #algo = DD.LagrangeDual()
 
     # Add Lagrange dual problem for each scenario s.
-    nodelist = get_stage_id(tree)
-    leafdict = leaf2block(nodelist[K])
+    nodelist = DD.get_stage_id(tree)
+    leafdict = DD.leaf2block(nodelist[K])
     models = Dict{Int,JuMP.Model}(id => create_scenario_model(K,L,tree,id) for id in nodelist[K])
     for id in nodelist[K]
         DD.add_block_model!(algo, leafdict[id], models[id])
@@ -255,7 +165,7 @@ function dual_decomp(L::Int, tree::Tree)
     coupling_variables = Vector{DD.CouplingVariableRef}()
     for k in 1:K-1
         for root in nodelist[k]
-            leaves = get_future(tree, root)
+            leaves = DD.get_future(tree, root)
             for id in leaves
                 model = models[id]
                 yref = model[:y]

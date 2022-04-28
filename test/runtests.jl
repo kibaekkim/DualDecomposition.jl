fc6033170898f8a260456dcee12a889fb67d1552
Hideaki Nakao <h.nakao1992blanca@gmail.com>
2022-04-28 14:10:02 -0500

    import random package

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,12 @@ using DualDecomposition
 using JuMP, Ipopt, GLPK
 using MPI
 using LinearAlgebra
+using Random
 
 const DD = DualDecomposition
 
+include("investment_dro.jl")
 include("farmer.jl")
 include("mpi.jl")
 include("investment.jl")
 include("heuristics.jl")
-include("investment_dro.jl")

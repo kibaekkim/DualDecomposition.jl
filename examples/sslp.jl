04da86453fcd176e6199f4d53d0922437e176431
Hideaki Nakao <h.nakao1992blanca@gmail.com>
2023-08-07 06:01:46 -0500

    fixed typos of original

--- examples/sslp.jl
+++ examples/sslp.jl
@@ -96,8 +96,11 @@ function main_sslp(nJ::Int, nI::Int, nS::Int, seed::Int=1)
     # Set nonanticipativity variables as an array of symbols.
     DD.set_coupling_variables!(algo, coupling_variables)
     
+    # Lagrange master method
+    LM = DD.BundleMaster(BM.ProximalMethod, optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0))
+
     # Solve the problem with the solver; this solver is for the underlying bundle method.
-    DD.run!(algo, optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0))
+    DD.run!(algo, LM)
 end
 
 main_sslp(10,50,50)
